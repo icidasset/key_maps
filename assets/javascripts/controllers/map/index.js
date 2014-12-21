13efe23894220246cabd40bc48a029d336f256a0
Steven Vandevelde <icid.asset@gmail.com>
2014-12-21 23:44:54 +0100

    Update

--- assets/javascripts/controllers/map/index.js
+++ assets/javascripts/controllers/map/index.js
@@ -1,4 +1,32 @@
 K.MapIndexController = Ember.Controller.extend({
+  fullWidthTypes: ["text"],
+
+
+  structure_arrays: function() {
+    var structure = JSON.parse(this.get("model.structure"));
+    var fwt = this.get("fullWidthTypes");
+    var full = [];
+    var all = [];
+
+    structure.forEach(function(s) {
+      var l = all.length === 0 ? undefined : all[all.length - 1];
+
+      if (fwt.contains(s.type)) {
+        full.push(s);
+      } else {
+        if (l === undefined || l.length >= 2) {
+          l = [];
+          all.push(l);
+        }
+
+        l.push(s);
+      }
+    });
+
+    return [all, [full]];
+  }.property("model.structure"),
+
+
   actions: {
 
     add: function() {

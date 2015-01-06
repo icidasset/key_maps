7228cac381953c9af9b6b90bafc527d6ef638965
Steven Vandevelde <icid.asset@gmail.com>
2015-01-06 10:58:38 +0100

    Public api update

--- api/public.go
+++ api/public.go
@@ -2,6 +2,7 @@ package api
 
 import (
   "encoding/base64"
+  "encoding/json"
   "github.com/go-martini/martini"
   "github.com/icidasset/key-maps/db"
   "github.com/martini-contrib/render"
@@ -11,14 +12,13 @@ import (
 
 
 func Public__Show(params martini.Params, r render.Render, w http.ResponseWriter) {
-  data, _ := base64.StdEncoding.DecodeString(params["hash"])
+  data, err := base64.StdEncoding.DecodeString(params["hash"])
   str := string(data[:])
 
   // params
   map_id, _ := strconv.ParseInt(str, 10, 0)
-  map_id = map_id / 25
 
-  // query
+  // map items
   map_items := []MapItem{}
 
   db.Inst().Select(
@@ -27,5 +27,19 @@ func Public__Show(params martini.Params, r render.Render, w http.ResponseWriter)
     map_id,
   )
 
-  r.JSON(200, map_items)
+  // collection
+  collection := make([]map[string]interface{}, 0)
+
+  for _, m := range map_items {
+    c := make(map[string]interface{})
+    err = json.Unmarshal([]byte(m.StructureData), &c)
+    collection = append(collection, c)
+  }
+
+  // render
+  if err != nil {
+    r.JSON(500, err.Error())
+  } else {
+    r.JSON(200, collection)
+  }
 }

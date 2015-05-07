c975e2d22d9c9f526b03e63ffda4462ba0d8b7ae
Steven Vandevelde <icid.asset@gmail.com>
2015-05-07 13:39:29 +0200

    Update cors.go

--- middleware/cors.go
+++ middleware/cors.go
@@ -4,7 +4,7 @@ import "github.com/labstack/echo"
 
 func Cors(h echo.HandlerFunc) echo.HandlerFunc {
 	return func(c *echo.Context) error {
-		c.Response.Header().Set("Access-Control-Allow-Methods", "GET")
+		c.Response.Header().Set("Access-Control-Allow-Methods", "OPTIONS, GET, POST, PUT, PATCH, DELETE")
 		c.Response.Header().Set("Access-Control-Allow-Origin", "*")
 
 		return h(c)

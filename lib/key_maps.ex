3e83fdaf2803195822569f2758afa10bc42f3f7a
Steven Vandevelde <icid.asset@gmail.com>
2016-08-22 00:11:47 +0200

    Slightly adjust supervisor

--- lib/key_maps.ex
+++ lib/key_maps.ex
@@ -2,14 +2,18 @@ defmodule KeyMaps do
   use Application
 
   def start(_type, _args) do
-    import Supervisor.Spec
+    import Supervisor.Spec, warn: false
 
     children = [
       worker(KeyMaps.Repo, []),
       worker(KeyMaps.Router, [])
     ]
 
-    Supervisor.start_link(children, strategy: :one_for_one)
+    Supervisor.start_link(
+      children,
+      strategy: :one_for_one,
+      name: KeyMaps.Supervisor
+    )
   end
 
 end

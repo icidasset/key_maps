2f2b14a162522054db177fcdb7c891c025b41d5f
Steven Vandevelde <icid.asset@gmail.com>
2016-08-22 10:07:21 +0200

    Postgres extension hstore

--- priv/repo/migrations/20160330174439_create_map_items.exs
+++ priv/repo/migrations/20160330174439_create_map_items.exs
@@ -2,6 +2,8 @@ defmodule KeyMaps.Repo.Migrations.CreateMapItems do
   use Ecto.Migration
 
   def change do
+    execute "CREATE EXTENSION IF NOT EXISTS hstore"
+
     create table(:map_items) do
       add :attributes, :map
       add :map_id, references(:maps)
